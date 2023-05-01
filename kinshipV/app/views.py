aa5e5253ffe784ae4a622309f82062dfac036f8d
Soham Samanta <soham@Sohams-MacBook-Air.local>
2023-05-01 12:29:45 +0530

    upv5

---
+++ kinshipV/app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from ml.runner
+# Create your views here.
+
+def index(req):
+    return render(req,'index.html')
+
+def landpage(req):
+    return render(req,'landing.html')
+
+def formpage(req):
+    return render(req,'form.html')
